2c8aad633142c123677226ba33a7422c6220e2f1
Spenser Steele <spenser.steele@gmail.com>
2014-02-25 13:25:41 -0600

    fixed

--- full/coffee_controller.py
+++ full/coffee_controller.py
@@ -73,10 +73,10 @@ def action(changePin, action):
 		time.sleep(0.2)
 		lines = read_temp_raw()
 		equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
+	if equals_pos != -1:
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+		temp_f = temp_c * 9.0 / 5.0 + 32.0
    if action == "on":
       # Set the pin high:
       GPIO.output(changePin, GPIO.HIGH)
