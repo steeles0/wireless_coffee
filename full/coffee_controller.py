6521a0f89f9d7c3e3ba6360adbaf7559b038d311
Spenser Steele <spenser.steele@gmail.com>
2014-02-26 00:16:39 -0600

    setup temp bar

--- full/coffee_controller.py
+++ full/coffee_controller.py
@@ -54,7 +54,7 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_c
 	
 temp = read_temp()
 
